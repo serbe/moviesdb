f6a232a7954dbf56ead24b0eb3091d8973f2f0ae
Valeriy Kostikov <mkoctuk@gmail.com>
2017-08-31 07:47:48 +0300

    check error

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 extern crate postgres;
 
-use postgres::{Connection, TlsMode};
-use db::{get_movies};
+use db::{get_movies, db_conn};
 
-mod types;
+mod models;
 mod db;
 
 fn main() {
-    let conn = Connection::connect("postgres://movuser:movpass@localhost:5432/movies", TlsMode::None).unwrap();
-    // println!("{:?}", conn);
+    let conn = match db_conn() {
+        Ok(c) => c,
+        Err(_) => return
+    };
 
     let movies = get_movies(&conn, 1);
 
-    println!("{:?}", movies);
-
-    // for row in &conn.query("select * from torrents", &[]).unwrap() {
-    //     let mut torrent = types::Torrent::new();
-    //     torrent.id = row.get("id");
-    //     torrent.movie_id = row.get("movie_id");
-    //     torrent.resolution = row.get("resolution");
-    //     println!("Torrent {} {:?} {:?}", torrent.id, torrent.movie_id, torrent.resolution);
-    // }
+    println!("{:?}", movies.unwrap().len());
 }
