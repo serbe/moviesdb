8108b6ab6bff63cfcccadfc48c9ce1195b516fd9
Valeriy Kostikov <mkoctuk@gmail.com>
2017-09-27 15:52:30 +0300

    add iron

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate postgres;
+extern crate iron;
 
+use iron::prelude::*;
+use iron::status;
 use db::{get_movies, db_conn};
 
 mod models;
@@ -12,6 +15,10 @@ fn main() {
     };
 
     let movies = get_movies(&conn, 1);
+
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("localhost:3000").unwrap();
 
     println!("{:?}", movies.unwrap().len());
 }
