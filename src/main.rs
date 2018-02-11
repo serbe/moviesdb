98f49ba3121475ae0e702078dbe8006959103050
Valeriy Kostikov <mkoctuk@gmail.com>
2018-02-11 11:30:47 +0300

    use hyper

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,23 @@
 //#![cfg_attr(feature="clippy", plugin(clippy))]
 
 extern crate postgres;
-// extern crate iron;
-
-// use iron::prelude::*;
-// use iron::status;
+extern crate hyper;
+extern crate futures;
 
 //use std::sync::{Arc, Mutex};
 
-use db::{get_movies, db_conn};
+// use db::{get_movies, db_conn};
+use hyper::server::{Http};
+use services::{HelloWorld};
 
 mod models;
 mod db;
+mod services;
 
 fn main() {
-    let conn = db_conn();
+    // let conn = db_conn();
 
-    let movies = get_movies(&conn, 1);
+    // let movies = get_movies(&conn, 1);
 
 //    let sdb: Arc<Mutex<Connection>> = Arc::new(Mutex::new(conn));
     // let mut router = router::Router::new();
@@ -27,5 +28,9 @@ fn main() {
     //     Ok(Response::with((status::Ok, "Hello World!")))
     // }).http("localhost:3000").unwrap();
 
-    println!("{:?}", movies.unwrap().len());
+    // println!("{:?}", movies.unwrap().len());
+
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+    server.run().unwrap();
 }
